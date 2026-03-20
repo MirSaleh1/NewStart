acd91d0746b58ba9878d04fd904075ba3ce175c9
MirSaleh1 <ndnvsaleh123@gmail.com>
2026-03-20 06:57:48 +0400

    Initial commit

--- src/lesson1/HelloWorldApp.java
+++ src/lesson1/HelloWorldApp.java
@@ -1,6 +1,10 @@
+package lesson1;
+
 public class HelloWorldApp {
     public static void main(String[] args) {
 
         System.out.println("Hello World!!!");
+        System.out.printf("%-10s","Saleh");
+
     }
 }
--- src/lesson1/Task1App.java
+++
@@ -1,9 +0,0 @@
-package lesson1;
-
-public class Task1App {
-
-    public static void main(String[] args) {
-
-
-    }
-}
