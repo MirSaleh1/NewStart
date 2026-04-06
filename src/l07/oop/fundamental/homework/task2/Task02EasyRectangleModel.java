2c9dd0a88ec1bf58213f790e85abe9001a5c1e6d
MirSaleh1 <ndnvsaleh123@gmail.com>
2026-04-06 08:32:11 +0400

    implement Rectangle class with area and perimeter methods

---
+++ src/l07/oop/fundamental/homework/task2/Task02EasyRectangleModel.java
@@ -0,0 +1,12 @@
+package l07.oop.fundamental.homework.task2;
+
+public class Task02EasyRectangleModel {
+
+    public static void main(String[] args) {
+
+        Rectangle rc1 = new Rectangle(5.5, 2.5);
+        Rectangle rc2 = new Rectangle(7.5, 3.5);
+        System.out.println(rc2.area());
+        System.out.println(rc1.perimeter());
+    }
+}
