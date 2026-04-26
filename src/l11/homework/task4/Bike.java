b16fe48ecd4cb8d8bf0c45d9d2bfb33c8af748ef
MirSaleh1 <ndnvsaleh123@gmail.com>
2026-04-26 04:05:03 +0400

    implement runtime polymorphism using Vehicle, Car and Bike

---
+++ src/l11/homework/task4/Bike.java
@@ -0,0 +1,10 @@
+package l11.homework.task4;
+
+public class Bike extends Vehicle{
+
+    @Override
+    public void move() {
+        System.out.println("bike move");
+    }
+
+}
---
+++ src/l11/homework/task4/Vehicle.java
@@ -0,0 +1,8 @@
+package l11.homework.task4;
+
+public class Vehicle {
+
+    public void move(){
+        System.out.println("Vehicle move");
+    }
+}
