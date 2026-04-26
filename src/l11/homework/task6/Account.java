a85cd2f02e13d34d2ece6d0a9d7316e9dea34774
MirSaleh1 <ndnvsaleh123@gmail.com>
2026-04-26 05:03:11 +0400

    compare dynamic and static binding using instance and static methods

---
+++ src/l11/homework/task6/Account.java
@@ -0,0 +1,11 @@
+package l11.homework.task6;
+
+public class Account {
+
+    public static void bankName(){
+        System.out.println("ABB");
+    }
+    public void info(){
+        System.out.println("Hello my name is Saleh");
+    }
+}
---
+++ src/l11/homework/task6/Task06Main.java
@@ -0,0 +1,11 @@
+package l11.homework.task6;
+
+public class Task06Main {
+
+    public static void main(String[] args) {
+
+        Account ref = new PremiumAccount();
+        ref.info();
+        ref.bankName();
+    }
+}
